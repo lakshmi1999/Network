eb43dedf55ed601af3c363a47b10a7cf1c8bcf0b
lakshmi1999 <40885304+lakshmi1999@users.noreply.github.com>
2018-07-11 07:04:30 +0530

    Update Simple_udpserver.py

--- Simple_udpserver.py
+++ Simple_udpserver.py
@@ -1,10 +1,11 @@
 # Network
 import socket
+#define the udp ip address and port number
 udpip="10.1.24.121"
 udpport=6002
-serverSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-serverSock.bind((udpip,udpport))
-print("server is waiting")
+serverSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)#create the datagram socket
+serverSock.bind((udpip,udpport))#Bind to ip and port
+print("server is waiting")#Listening for incoming datagrams
 while True:
   data,addr=serverSock.recvfrom(1024)
   print("Message",data)
