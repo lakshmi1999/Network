5eb450168d12d0e22f592f5e9f32f49fdbf47373
lakshmi1999 <40885304+lakshmi1999@users.noreply.github.com>
2018-07-11 07:23:56 +0530

    Update echoserver.py

--- echoserver.py
+++ echoserver.py
@@ -1,11 +1,13 @@
-import socket
+#client socket
+import socket#import socket module
+#define the udp ip and port number
 udpip="10.1.24.121"
 udpport=6002
-serverSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-serverSock.bind((udpip,udpport))
-print("server is waiting")
+serverSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)#create a datagram socket
+serverSock.bind((udpip,udpport))#bind to ip and port
+print("server is waiting")#Listening for incoming datagrams
 while True:
-	cdata,addr=serverSock.recvfrom(1024)
+	cdata,addr=serverSock.recvfrom(1024)#receive the client packets along with the address it is coming from
 	print(cdata)
 	print(addr)
-	serverSock.sendto(cdata,addr)
+	serverSock.sendto(cdata,addr)#send the data to the client
