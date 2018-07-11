5c36d16482f62c1eecd6d88dcfad16a3e9acadc4
lakshmi1999 <40885304+lakshmi1999@users.noreply.github.com>
2018-07-11 07:24:15 +0530

    Update echoclient.py

--- echoclient.py
+++ echoclient.py
@@ -1,4 +1,5 @@
-import socket
+#echo client
+import socket#import socket module
 #here we define the udp ip address as well as port number
 udpip="10.1.24.122"
 udpport=6789
@@ -6,6 +7,6 @@ Message=("hii from client")
 bytesToSend=str.encode(Message)
 clientSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)#create a datagram socket
 clientSock.sendto(bytesToSend,(udpip,udpport))#send the data to server
-data=clientSock.recvfrom(1024)#message received from server
+data=clientSock.recvfrom(1024)#message received from server along with the address it is coming from
 print(data)
        
