5fb556a0a55e6b71b38ce0bdb49fbdefc9f82730
lakshmi1999 <40885304+lakshmi1999@users.noreply.github.com>
2018-07-13 15:39:22 +0530

    Create halfclient.py

---
+++ halfclient.py
@@ -0,0 +1,14 @@
+#half duplex client
+import socket
+serverport = 6004
+clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientsocket.connect(('10.1.24.121',serverport))
+while True:
+  sentence = input(">> ")
+  clientsocket.send(sentence.encode())
+  message = clientsocket.recv(2048)
+  print (">> ", message.decode())
+  if(sentence == "disconnect"):
+    break
+print("BYEE SERVER")
+clientsocket.close()
