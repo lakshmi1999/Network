8b2d38ae5d313f8769b03ccd5867ac591cac56c0
lakshmi1999 <40885304+lakshmi1999@users.noreply.github.com>
2018-09-18 11:37:48 +0530

    Create sender_sw.py

---
+++ sender_sw.py
@@ -0,0 +1,13 @@
+import socket
+client= socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+client.bind(('127.0.0.1',5011))
+dest=('127.0.0.1',5100)
+user=input(">> ")
+user_input=str.encode(user)
+while user_input:
+	client.sendto(user_input,dest)
+	val,address = client.recvfrom(2048)
+	print ("EXPECTING FRAME:"+str(val))
+	user=input(">> ")
+	user_input=str.encode(user)
+client.close()
