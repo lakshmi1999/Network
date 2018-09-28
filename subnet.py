6154f2c54092287c4918eb8fa99007d9427cfd63
lakshmi1999 <40885304+lakshmi1999@users.noreply.github.com>
2018-09-28 15:19:48 +0530

    Update subnet.py

--- subnet.py
+++ subnet.py
@@ -1,10 +1,16 @@
 import ipaddress
-addr=input("Enter the ip ")
-#input("Enter the prefix")
+import math
+addr=input("Enter the ip:")
+#host=ipaddress.ip_address(addr)
 ip=addr.split("/")
-print(ip[1])
-print(addr)
-n=ipaddress.IPv4Network('145.0.0.0/8')
-first,last=n[0],n[-1]
-print(first)
-print(last)
+p=ip[1]
+d=32-int(p)
+try:
+	m=math.pow(2,d)
+	n=ipaddress.IPv4Network(addr)
+	first,last=n[0],n[-1]
+	print("ADDRESS BLOCK:",int(m))
+	print("FIRST ADDRESS:",first)
+	print("LAST ADDRESS:",last)
+except:
+	print("INVALID HOST BITS")
